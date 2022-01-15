25b1dcbc62d3ebcfdb57fc1c4b2430c2ba50d3d6
raylexlee <raylexym57lee@gmail.com>
2022-01-16 00:12:16 +0800

    Updated entries

---
+++ 104.sh
@@ -0,0 +1,23 @@
+BEGIN=$(date)
+./novel_maketext 韓寒 青春 http://365novel.com/book/4601/
+./novel_maketext 韓寒 1988：我想和這個世界談談 http://365novel.com/files/chinese/201101/1009.html
+./novel_maketext 韓寒 他的國 http://365novel.com/files/chinese/201101/1008.html
+./novel_maketext 韓寒 三重門 http://365novel.com/book3/7384/
+./novel_maketext 韓寒 可愛的洪水猛獸 http://365novel.com/files/chinese/200909/842.html
+./novel_maketext 韓寒 光榮日（第一季） http://365novel.com/files/chinese/201101/979.html
+./novel_maketext 韓寒 雜的文 http://365novel.com/files/chinese/201101/1007.html
+./novel_maketext 韓寒 長安亂 http://365novel.com/book3/7385/
+./novel_maketext 韓寒 一座城池 http://365novel.com/book3/7378/
+./novel_maketext 韓寒 我所理解的生活 http://365novel.com/book4/10384/
+./novel_maketext 韓寒 像少年啦飛馳 http://365novel.com/book3/7379/
+./novel_maketext 韓寒 零下一度 http://365novel.com/book3/7380/
+./novel_maketext 韓寒 毒 http://365novel.com/book3/7382/
+./novel_maketext 韓寒 毒2 http://365novel.com/book3/7383/
+./novel_maketext 韓寒 通稿2003 http://365novel.com/book3/7381/
+./novel_maketext 韓寒 就這麼漂來漂去 http://365novel.com/files/chinese/201101/1006.html
+./novel_maketext 韓寒 那麼紅 http://365novel.com/files/chinese/201101/1004.html
+./novel_maketext 韓寒 草 http://365novel.com/files/chinese/201101/1005.html
+./novel_maketext 韓仁均 兒子韓寒 http://365novel.com/files/chinese/201101/1003.html
+./novel_maketext 方肇 韓寒--最好的年代 http://365novel.com/book3/6138/
+echo $BEGIN
+date

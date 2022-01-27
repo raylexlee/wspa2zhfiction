4179cac62e0678a5c9de53c8ab9f287ee463f267
Raylex Lee <raylexym57lee@gmail.com>
2022-01-27 14:14:17 +0800

    Update entries.

---
+++ 1583.sh
@@ -0,0 +1,6 @@
+./novel_maketext 喬治·盧卡斯 星球大戰1前傳：魅影危機 http://365novel.com/files/sf/201102/1587.html
+./novel_maketext 喬治·盧卡斯 星球大戰2前傳：克隆人的進攻 http://365novel.com/files/sf/201102/1588.html
+./novel_maketext 喬治·盧卡斯 星球大戰3前傳：西斯的復仇 http://365novel.com/files/sf/201102/1589.html
+./novel_maketext 喬治·盧卡斯 星球大戰4：新希望 http://365novel.com/files/sf/201102/1584.html
+./novel_maketext 喬治·盧卡斯 星球大戰5：帝國反擊戰 http://365novel.com/files/sf/201102/1585.html
+./novel_maketext 喬治·盧卡斯 星球大戰6：絕地歸來 http://365novel.com/files/sf/201102/1586.html
---
+++ 1651.sh
@@ -0,0 +1,3 @@
+./novel_maketext 阿瑟·克拉克 與拉瑪會合 http://365novel.com/files/sf/201102/1653.html
+./novel_maketext 阿瑟·克拉克 天堂裏的噴泉 http://365novel.com/files/sf/201102/1654.html
+./novel_maketext 阿瑟·克拉克 海豚島 http://365novel.com/files/sf/201102/1652.html

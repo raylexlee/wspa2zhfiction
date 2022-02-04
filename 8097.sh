0d1f21faa8b7ba10a39799c9ac07adf8bafd5364
Raylex Lee <raylexym57lee@gmail.com>
2022-02-04 17:52:56 +0800

    Update entries.

---
+++ 8097.sh
@@ -0,0 +1,4 @@
+./novel_maketext 福爾摩斯 新探案 http://365novel.com/book3/8100/
+./novel_maketext 福爾摩斯 歸來記 http://365novel.com/book3/8101/
+./novel_maketext 福爾摩斯 冒險史 http://365novel.com/book3/8108/
+./novel_maketext 福爾摩斯 回憶錄 http://365novel.com/book3/8099/
--- followup.sh
+++ followup.sh
@@ -1,5 +1,6 @@
-./get_text 九月奔雷 005
-./get_text 九月奔雷 006
-./get_text 九月奔雷 007
-./get_text 九月奔雷 008
-./get_text 九月奔雷 009
+./get_text 陷阱 008
+./get_text 遺囑 024
+./get_text 超級說客 023
+./get_text 超級說客 024
+./get_text 失控的陪審團 003
+./get_text 失控的陪審團 041

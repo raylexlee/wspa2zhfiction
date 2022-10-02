bcd7a8f4ec3c5907db912e396ab84025c406eba4
Raylex Lee <raylexym57lee@gmail.com>
2022-10-03 06:34:30 +0800

    Automate  scrooll to bottom for dog9net

--- dog9.py
+++ dog9.py
@@ -2,13 +2,24 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import sys
+import time
 
 PATH="C:\Program Files (x86)\chromedriver.exe"
+def ScrollPageToBottom(html):
+  i = 0
+  while i < 3:
+    scrollHeight = html.get_property('scrollHeight')
+    html.send_keys(Keys.PAGE_DOWN)
+    time.sleep(.5)
+    if scrollHeight < html.get_property('scrollHeight'): 
+      i = 0 
+    else: 
+      i = i + 1
 def getLogfile(filename, link):
   driver = webdriver.Chrome(PATH)
   driver.get(link)
   body = driver.find_element(By.TAG_NAME, "body")
-  input("Press Enter to get "+filename)
+  ScrollPageToBottom(body)
   innerText = body.get_property('innerText')
   f = open(filename, "w", encoding='utf-8')
   f.write(innerText)

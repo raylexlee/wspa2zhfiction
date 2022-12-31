280835b578eea9f80f8fdbe4cd6b8ea05b8b5259
Raylex Lee <raylexym57lee@gmail.com>
2022-12-31 19:37:57 +0800

    Translated from split_dog9chapter.js

--- evenly_split.sh
+++ evenly_split.sh
@@ -9,6 +9,29 @@ do
 #echo $i ${a[$i]}
 SIZE=$(( $SIZE + ${#a[$i]} ))
 done
-let N=(SIZE+MAX-1)/MAX
-let max=SIZE/s
-
+let NFILES=(SIZE+MAX-1)/MAX
+let NMAX=(SIZE+NFILES-1)/NFILES
+#echo $MAX $SIZE $NFILES $NMAX
+let START=1
+let j=1
+SPLITNAME="$CHAPTER"_$j
+[ $NFILES -eq 1 ] && SPLITNAME="$CHAPTER"__
+let n=0
+for i in ${!a[@]}
+do
+L=${#a[$i]}
+a[$i]="${a[$i]}\n"
+let FINAL=i
+let n=n+L 
+if [ $n -ge $NMAX ]; then
+echo -e ${a[@]:$START:$FINAL} i > $SPLITNAME
+let j=j+1
+SPLITNAME="$CHAPTER"_$j
+[ $NFILES -eq 1 ] && SPLITNAME="$CHAPTER"__
+let START=FINAL+1
+let n=0
+fi
+done
+if [ $START -le ${#a[@]} ]; then
+echo -e ${a[@]:$START:$FINAL} > $SPLITNAME
+fi

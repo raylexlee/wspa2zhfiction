1970a36cc1f91a3fb47e887f220af7b25c8f1afc
Raylex Lee <raylexym57lee@gmail.com>
2024-02-04 00:51:19 +0800

    Use markdown as input

--- genEpub.sh
+++ genEpub.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-AUTHOR=${1-金庸}
+AUTHOR=${1-思高聖經}
 LANG=${2-zho}
 BOOK=$(pwd | sed 's#.*/\([^/]*\)#\1#')
-TXT=$BOOK.txt
+TXT=$BOOK.md
 cp /dev/null $TXT
-for i in $(ls [0-9]*txt)
-do
-  cat $i >> $TXT
+for i in $(ls [0-9]*txt) 
+do 
+j=$(echo $i | tr -d .tx)
+echo $(grep ^$j coverparameters.txt | sed 's/^[0-9]*/####/') >> $TXT
+sed 's/$/\n/' $i >> $TXT 
 done
-ebook-convert $TXT $BOOK.epub --authors $AUTHOR --language $LANG
+ebook-convert $TXT $BOOK.epub --title $BOOK --authors $AUTHOR --language $LANG --level1-toc //h:h4

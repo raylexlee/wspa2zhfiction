07949b05e93362ca07ee3e9e649b4ba3ff7119f4
Raylex Lee <raylexym57lee@gmail.com>
2024-08-14 13:26:14 +0800

    Refine yodu tools

---
+++ getAllBooksYoduAuthor.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+GETLINK=$HOME/wspa2zhfiction/getlinks.js
+AUTHOR=${1?'missing name of author ...'}
+ALLBOOKS=yodu_triples.txt
+[ -f $ALLBOOKS ] || touch $ALLBOOKS
+for PAGE in $(seq 1000)
+do
+LINK="https://tw.yodu.org/modules/article/authorarticle.php?author=""$AUTHOR""&page=""$PAGE"
+BOOKS=$($GETLINK "$LINK" | grep [^.][^.].https.*for-author$)
+if [ "$BOOKS" = "" ]; then
+   break
+else
+   echo $BOOKS | sed 's#for-author #for-author\n#g' | sed '/^[^h]\{21,\}/ d' | sed 's#\([^ ]*\)\s.*book.\([0-9]*\)..for.*#'"$AUTHOR"' \1 \2#'
+fi 
+done >> $ALLBOOKS
+rm -rf 'C:'*
